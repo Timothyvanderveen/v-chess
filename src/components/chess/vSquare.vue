cab41bf5509a7f6002837154ae82725c9d2fd639
Timothy <Timothyvanderveen@gmail.com>
2023-07-07 19:24:01 +0200

    use translate instead of top left

--- src/components/chess/vSquare.vue
+++ src/components/chess/vSquare.vue
@@ -3,51 +3,55 @@
     ref="squareElement"
     class="v-square"
     :class="getColourClass()"
-    :style="{ left: getLeft(), bottom: getBottom() }"
+    :style="{ translate: getTranslate() }"
   >
     {{ file }} - {{ rank }}
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, type PropType, type Ref } from 'vue'
+import { ref, type PropType, type Ref } from "vue";
 
-const squareElement = ref(null) as Ref<HTMLElement | null>
+const squareElement = ref(null) as Ref<HTMLElement | null>;
 
-const rankArray = Array.from(Array(8).keys()).map((e) => ++e)
-const fileArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+const rankArray = Array.from(Array(8).keys()).map((e) => ++e);
+const fileArray = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 const props = defineProps({
   id: {
     type: Number,
-    required: true
+    required: true,
   },
   file: {
     type: String as PropType<vSquareFile>,
-    required: true
+    required: true,
   },
   rank: {
     type: Number as PropType<vSquareRank>,
-    required: true
+    required: true,
   },
   colour: {
     type: String as PropType<vSquareColour>,
     required: true,
-    validator: (prop: string) => ['black', 'white'].includes(prop)
-  }
-})
+    validator: (prop: string) => ["black", "white"].includes(prop),
+  },
+});
+const getTranslate = () => {
+  const x = fileArray.indexOf(props.file) * 100;
+  const y = (8 - props.rank) * 100;
+  return `${x}% ${y}%`;
+};
+// const getBottom = () => {
+//   if (!squareElement.value) return "0";
+//   return (props.rank - 1) * squareElement.value.offsetWidth + "px";
+// };
 
-const getBottom = () => {
-  if (!squareElement.value) return '0'
-  return (props.rank - 1) * squareElement.value.offsetWidth + 'px'
-}
+// const getLeft = () => {
+//   if (!squareElement.value) return "0";
+//   return fileArray.indexOf(props.file) * squareElement.value.offsetWidth + "px";
+// };
 
-const getLeft = () => {
-  if (!squareElement.value) return '0'
-  return fileArray.indexOf(props.file) * squareElement.value.offsetWidth + 'px'
-}
-
-const getColourClass = () => `v-square__colour--${props.colour}`
+const getColourClass = () => `v-square__colour--${props.colour}`;
 </script>
 
 <style lang="scss">
