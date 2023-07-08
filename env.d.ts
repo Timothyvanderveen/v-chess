c874761287b6ca3ff65b1b88bca5231979f76df5
Timothy <Timothyvanderveen@gmail.com>
2023-07-08 13:32:25 +0200

    more types

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,30 @@
 /// <reference types="vite/client" />
 // vSquare
 
-type vSquareColour = 'black' | 'white'
-type vSquareFile = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h'
-type vSquareRank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+type vSquareColour = "black" | "white";
+type vPlayerColour = "black" | "white";
+type vSquareFileLetter = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h";
+type vSquareFileNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+type vSquareRankNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+type vPieceType =
+  | "r"
+  | "R"
+  | "b"
+  | "B"
+  | "n"
+  | "N"
+  | "q"
+  | "Q"
+  | "k"
+  | "K"
+  | "p"
+  | "P";
+
+interface vSquareState {
+  squareIndex: number;
+  piece: vPieceType | null;
+}
+
+interface vBoardState {
+  [key: number]: vSquareState;
+}
