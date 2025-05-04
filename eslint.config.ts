12f76f91342a99d10eaacab8b1a893da2ce6c55a
Timothy <Timothyvanderveen.dev@gmail.com>
2025-05-04 13:29:00 +0200

    fix package managing

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,33 +1,14 @@
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
-import pluginVitest from '@vitest/eslint-plugin'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVue from 'eslint-plugin-vue';
+import pluginVitest from '@vitest/eslint-plugin';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
 
 export default defineConfigWithVueTs(
+  pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  stylistic.configs.recommended,
-  pluginVue.configs.recommended,
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  {
-    rules: {
-      'indent': ['error', 2],
-      '@stylistic/indent': ['error', 2],
-      '@stylistic/function-call-argument-newline': ['error', 'consistent'],
-      '@stylistic/object-curly-newline': ['error', { consistent: true }],
-      '@stylistic/semi': ['error', 'always'],
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-        },
-      ],
-    },
-  },
-)
+  skipFormatting,
+);
